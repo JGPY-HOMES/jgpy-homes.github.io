8f0f958c113f688d11c23b9d0311f327ac7736c4
hjm100 <hjm100@126.com>
2025-02-17 11:20:51 +0800

    feat: 官网项目初始化

---
+++ static/js/map.js
@@ -0,0 +1,1 @@
+(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/api?v=3.0&ak=fQbZZuj4L9ws01ra9aEoxQeyvuYIyNj1"></script>');})();
